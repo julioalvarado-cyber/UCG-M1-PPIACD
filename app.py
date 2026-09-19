2d84804cb63fdd795e9e27b573ba703948ae05d8
julioalvarado-cyber <julio.alvarado@casagrande.edu.ec>
2026-09-19 11:32:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ st.image("LogoUCG.jpeg")
 st.sidebar.title("Paradigmas de programación para IA & CD")
 st.write("Elaborado por: Julio Alvarado")
 
-capital=st.number_input("Ingrese el capital")
-taza_anual_pct=st.number_input("Ingrese el taza")
-dias_mora=st.number_input("Ingrese los dias de mora")
+capital = st.number_input("Ingrese el capital", value = 1000)
+taza_anual_pct = st.number_input("Ingrese el taza", value + 15)
+dias_mora = st.number_input("Ingrese los dias de mora", value = 20)
 
-#resultado = lf.calcular_interes_mora()
+resultado = lf.calcular_interes_mora(capital,taza_anual_pct,dias_mora)
+
+st.write("El resultado por atrazo es: ", resultado)
