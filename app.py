7fd5b0a2d5d571663a14b8b4c07dd75a26e431f4
julioalvarado-cyber <julio.alvarado@casagrande.edu.ec>
2026-09-19 11:29:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import libreria_funciones as lf
 
 st.title("Maestría en Ciencias de Datos - Universidad Casa Grande")
 st.image("LogoUCG.jpeg")
 
 st.sidebar.title("Paradigmas de programación para IA & CD")
 st.write("Elaborado por: Julio Alvarado")
+
+capital=st.number_input("Ingrese el capital")
+taza_anual_pct=st.number_input("Ingrese el taza")
+dias_mora=st.number_input("Ingrese los dias de mora")
+
+resultado = lf.calcular_interes_mora()
