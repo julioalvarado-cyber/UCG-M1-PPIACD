9e63f67147479a0680b572212657b201b7757301
julioalvarado-cyber <julio.alvarado@casagrande.edu.ec>
2026-09-19 11:15:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.title("Maestría en Ciencias de Datos - Universidad Casa Grande")
 st.sidebar.title("Paradigmas de programación para IA & CD")
+st.write("Elaborado por: Julio Alvarado")
